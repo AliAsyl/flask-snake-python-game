779f7c7882c22314929882c2de6802570fb31451
AliAsyl <s30939@pjwstk.edu.pl>
2025-04-01 21:40:38 +0200

    Created testing functions, and corresponding functions to be tested

---
+++ game_engine/engine.py
@@ -0,0 +1,22 @@
+
+
+def move_object(position, direction):
+    x, y = position
+    if direction == "up":
+        return (x, y - 1)
+    elif direction == "down":
+        return (x, y + 1)
+    elif direction == "left":
+        return (x - 1, y)
+    elif direction == "right":
+        return (x + 1, y)
+    else:
+        raise ValueError("Invalid direction")
+
+def check_collision(pos1, pos2):
+    return pos1 == pos2
+
+def is_within_bounds(position, board_size):
+    x, y = position
+    w, h = board_size
+    return 0 <= x < w and 0 <= y < h
