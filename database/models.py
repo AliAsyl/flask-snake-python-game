460bf4c31823450bc65f62f095b26ce52678c684
AliAsyl <s30939@pjwstk.edu.pl>
2025-04-15 21:36:15 +0200

    Renamed GameRecord to ScoreRecord. Added static load function, as soon as the each player will have multiple scores. TABLE string is now a static property of each class. Model now has an override method get_as_json for all it inheriting objects, to make mongo jsons easilly.

--- database/models.py
+++ database/models.py
@@ -1,35 +1,37 @@
 from database.db import Database
-
+import datetime
 
 
 class Model:
-    def __init__(self, table):
-        self.table = table
+    TABLE = ""
 
     def model_create(self):
-        Database.create(self.table, self.get_as_json())
+        Database.create(Model.TABLE, self.get_as_json())
 
     def model_update(self):
-        Database.update(self.table, self.get_as_json())
+        Database.update(Model.TABLE, self.get_as_json())
 
     def get_as_json(self):
         return {}
 
 
 class Player(Model):
+    TABLE = "players"
     def __init__(self, name):
-        super().__init__("players")
+        super().__init__()
         self.name = name
         self.total_score = 0
+        self.last_session_time = ""
     
     def get_as_json(self):
         return {
             'name':self.name,
-            'total_score':self.total_score
+            'total_score':self.total_score,
+            'last_session_time':self.last_session_time
         }
 
-    def load(self, name):
-        result = Database.read(self.table, {'name':name})
+    def load(self):
+        result = Database.read(Player.TABLE, {'name':self.name})
         if len(result) == 0:
             self.model_create()
             return
@@ -39,17 +41,31 @@ class Player(Model):
         self.total_score += score
         self.model_update()
     
-class GameRecord(Model):
-    def __init__(self, player_name, score, collected_berries):
-        super().__init__("scores")
+class ScoreRecord(Model):
+    TABLE = "scores"
+    def __init__(self, player_name, score, collected_berries, time):
+        super().__init__()
         self.player_name = player_name
         self.score = score
         self.collected_berries = collected_berries
+        self.time = time
+
+    def get_as_json(self):
+        return {
+            'player_name':self.player_name,
+            'score':self.score,
+            'collected_berries':self.collected_berries,
+            'time':self.time
+        }
+
 
     @staticmethod
     def load(player_name):
-        search_results = Database.read(self.table, {'player_name':player_name})
+        search_results = Database.read(ScoreRecord.TABLE, {'player_name':player_name})
         records = []
         for record in search_results:
-            records.append(GameRecord(player_name, record['score'], record['collected_berries']))
+            records.append(ScoreRecord(player_name, record['score'], record['collected_berries'], record['time']))
         return records
+    
+    def save(self):
+        Database.create(ScoreRecord.TABLE, self.get_as_json())
