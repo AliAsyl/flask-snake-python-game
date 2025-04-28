ded07500b58b2b2189a1982801b58a02474f3185
AliAsyl <s30939@pjwstk.edu.pl>
2025-04-28 20:27:10 +0200

    Added /api/game/state GET request, which returns the state of all game objects in a game

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from engine.entities import Cat, Berry
+from engine.entities import Cat, Berry, GameObject
 from engine.core import Vector2D
 from database.db import Database
 
@@ -9,9 +9,44 @@ from flask_cors import CORS
 import random
 
 
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/game/state', methods=['GET'])
+def game_state():
+    response = {
+        "cat":{},
+        "berries":[]
+    }
+    
+    for obj in GameObject.GAME_OBJECTS:        
+        if isinstance(obj, Cat):
+            response["cat"] = {
+                "x":obj.hitbox.position.x,
+                "y":obj.hitbox.position.y,
+                "score": obj.collected_points,
+                "berries_collected": obj.collected_berries,
+                "berries_required": obj.berries_to_collect,
+                "game_over": obj.collected_berries >= obj.berries_to_collect
+            }
+        elif isinstance(obj, Berry):
+            response["berries"].append({
+                "x": obj.hitbox.position.x, 
+                "y": obj.hitbox.position.y
+            })
+    return jsonify(response)
+
+
+
+
 def main():
     Database.init()
     cat = Cat(Vector2D(200, 200), 5)
+    app.run(debug=True)
+
 
 if __name__ == "__main__":
     main()
