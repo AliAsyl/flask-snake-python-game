58962672f882164c5478ce2c90bc06eefd7894aa
AliAsyl <s30939@pjwstk.edu.pl>
2025-04-28 20:17:10 +0200

    Deleted the old ui function from main.py

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 from engine.entities import Cat, Berry
-from ui.ui import GameUI
 from engine.core import Vector2D
 from database.db import Database
 
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+
 import random
 
-def on_frame_randered():
-    if random.random() < 0.01:
-        Berry(Vector2D(
-            random.randint(
-                Berry.BERRY_HITBOX_WIDTH, 
-                GameUI.SCREEN_WIDTH - Berry.BERRY_HITBOX_WIDTH
-            ), 
-            random.randint(
-                Berry.BERRY_HITBOX_HEIGHT,
-                GameUI.SCREEN_HEIGHT - Berry.BERRY_HITBOX_HEIGHT
-            )
-        ))
 
 def main():
     Database.init()
-    cat = Cat(Vector2D(GameUI.SCREEN_WIDTH//2, GameUI.SCREEN_HEIGHT//2),   5)
-    ui = GameUI(cat, on_frame_randered)
-
-    if ui.init_cli():
-        ui.init_gui()
-        ui.run()
+    cat = Cat(Vector2D(200, 200), 5)
 
 if __name__ == "__main__":
     main()
