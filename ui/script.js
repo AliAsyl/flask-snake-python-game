fb531338c4a94ea4f37130ae3057afc1e02f5498
AliAsyl <s30939@pjwstk.edu.pl>
2025-04-28 20:06:40 +0200

    Added function templates at script.js

---
+++ ui/script.js
@@ -0,0 +1,13 @@
+const API_BASE = 'http://localhost:5000/api';
+
+function listPlayers() {}
+
+function showForm() {}
+
+function startNewGame() {}
+
+function showScoreForm() {}
+
+function showScores() {}
+
+function quitGame() {}
