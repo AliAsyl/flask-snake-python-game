aa96d4776a4f5a6d99506ad31f2c7855e8f4f8c6
AliAsyl <s30939@pjwstk.edu.pl>
2025-06-04 15:28:49 +0200

    Fixed tail elements spawning

--- engine/entities.py
+++ engine/entities.py
@@ -15,24 +15,19 @@ class Cat(GameObject):
         self.collected_points = 0
         self.berries_to_collect = berries_to_collect
         self.tail = []
-        self.last_tail_position = None
 
 
     def move(self, board):
         future_hitbox = self.hitbox.copy()
         future_hitbox.position += self.move_direction
         if board.is_inner_rect(future_hitbox):
-            if len(self.tail) > 0:
-                self.last_tail_position = self.tail[-1].hitbox.copy().position
-                for i in range(len(self.tail) - 1, -1, -1):
-                    if i == 0:
-                        self.tail[0].hitbox.position = self.hitbox.copy().position
-                    else:
-                        self.tail[i - 1].hitbox.position = self.tail[i].hitbox.position
-            else:
-                self.last_tail_position = self.hitbox.copy().position
+            for i in range(len(self.tail) - 1,-1,-1):
+                if i == 0:
+                    self.tail[i].hitbox.position = self.hitbox.copy().position
+                else:
+                    self.tail[i].hitbox.position = self.tail[i - 1].hitbox.position
             self.move_and_collide(self.move_direction)
-
+            #2
             return True
         return False
 
@@ -40,8 +35,7 @@ class Cat(GameObject):
         if isinstance(other, Berry):
             self.collected_berries += 1
             self.collected_points += other.points
-            self.tail.append(Tail(self.last_tail_position))
-
+            self.tail.append(Tail(self.hitbox.copy().position))
             other.dispose()
 
 class Berry(GameObject):
